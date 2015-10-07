17cc3309d3d1ba7b112b15620ce6654f517b783c
unknown <bkuhlman@PC-EVT-109B.global.tektronix.net>
2015-10-07 14:18:53 -0700

    new js file

---
+++ module1/bottles.js
@@ -0,0 +1,15 @@
+﻿var count = 99;
+var word = "bottles";
+var mms = 10;
+while ( count > 0 ) {
+  console.log(count + " " + word + " of rootbeer on the wall");
+  console.log(count + " " + word + " of rootbeer,");
+  console.log("Take one down, pass it around,");
+  count--;
+  if (count > 0) {
+   console.log(count + " " + word + " of rootbeer on the wall.");
+  } else {
+   console.log("No " + word + " of rootbeer on the wall.");
+  }
+}
+
