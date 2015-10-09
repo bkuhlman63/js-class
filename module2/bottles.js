152e6cfe02b79818f5b81918926e0f5e4ab09344
Brad Kuhlman <bradk_63@yahoo.com>
2015-10-09 15:05:09 -0700

    new files for module 2, lesson 1

---
+++ module2/bottles.js
@@ -0,0 +1,26 @@
+﻿var count = 99;
+var word = "bottles";
+while ( count > 0 ) {
+  if (count > 1) {
+	console.log(count + " " + word + " of beer on the wall");
+	console.log(count + " " + word + " of beer,");
+  } else {
+	// remove plural from word
+	console.log(count + " " + word.substring(0,word.length-1) + " of beer on the wall");
+	console.log(count + " " + word.substring(0,word.length-1) + " of beer,");
+  }
+  console.log("Take one down, pass it around,");
+  count--;
+  if (count > 1) {
+	console.log(count + " " + word + " of beer on the wall.");
+  } else if (count == 1) {
+	// remove plural from word
+	console.log(count + " " + word.substring(0,word.length-1) + " of beer on the wall.");
+  } else
+  {
+	console.log("No " + word + " of beer on the wall.");
+  }
+  console.log("");
+
+}
+
